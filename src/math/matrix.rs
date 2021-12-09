7b2488419ae235abd29d4633d1fbb2cb33857577
Jacob <jjq.quirky@gmail.com>
2021-12-08 23:19:50 -0500

    adding matrix index

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -8,6 +8,54 @@ pub struct Mat2 {
     pub z: Vec3,
 }
 
+impl Index<usize> for Mat2 {
+    type Output = Vec3;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("Index out of range!"),
+        }
+    }
+}
+
+impl IndexMut<usize> for Mat2 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            _ => panic!("Index out of range!"),
+        }
+    }
+}
+
+impl Index<usize> for Mat3 {
+    type Output = Vec4;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("Index out of range!"),
+        }
+    }
+}
+
+impl IndexMut<usize> for Mat3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            _ => panic!("Index out of range!"),
+        }
+    }
+}
+
 pub struct Mat3 {
     pub x: Vec4,
     pub y: Vec4,
